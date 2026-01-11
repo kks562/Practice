425e0cfc387ca60acc7eec3535e83b272b3211f5
kks562 <kavin.krishnas.562@gmail.com>
2026-01-11 15:16:19 +0530

    added more styles

--- script.js
+++ script.js
@@ -3,5 +3,17 @@ var a=document.getElementById("k1");
 a.addEventListener('click',function(){
     a.style.color = "red";
 a.style.backgroundColor = "yellow";
-a.style.fontSize = "20px";  
+a.style.fontSize = "20px";
+
+/* more styles 👇 */
+a.style.fontWeight = "bold";
+a.style.fontFamily = "Arial";
+a.style.padding = "10px";
+a.style.margin = "20px";
+a.style.border = "2px solid black";
+a.style.borderRadius = "10px";
+a.style.textAlign = "center";
+a.style.cursor = "pointer";
+a.style.boxShadow = "2px 2px 10px gray";
+ 
 })
